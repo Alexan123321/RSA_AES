8b7c221529004f23fae7dd58341370ddf9fdf416
Alexander Johansen <60431697+Alexan123321@users.noreply.github.com>
2021-09-28 09:11:44 +0200

    Add files via upload

--- RSA/src/main.go
+++ RSA/src/main.go
@@ -13,24 +13,31 @@ section 5.2.1 by Ivan Damgaard, Jesper Buus Nielsen & Claudio Orlandi.
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
-	"math/rand"
-	"strconv"
-	"time"
+	"math/big"
 	"week_4/src/packages/rsaexample"
 )
 
 func main() {
-	/* Generate random k */
-	rand.Seed(time.Now().UnixNano())
-	k := rand.Int()
+	/* Generate pseudo-random k */
+	max := new(big.Int)
+	max.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(max, big.NewInt(1))
+	k, err := rand.Int(rand.Reader, max)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	e := 3 //CHANGE ME
 
 	/* Generate public and private key, respectively */
 	publicKey, privateKey := rsaexample.KeyGen(k, e)
 
-	m := 69 //CHANGE ME
+	/* Generate random message */
+	m, err := rand.Int(rand.Reader, publicKey.N)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	/* Encrypt m with the public key */
 	c := rsaexample.Encrypt(m, publicKey)
@@ -39,6 +46,6 @@ func main() {
 	decrypted := rsaexample.Decrypt(c, privateKey)
 
 	/* Print message before encryption and after decryption */
-	fmt.Println("Message before encryption: " + strconv.Itoa(m))
+	fmt.Println("Message before encryption: " + m.String())
 	fmt.Println("Message after decryption: " + decrypted)
 }
